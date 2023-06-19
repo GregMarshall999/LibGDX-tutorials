53c8938565fa0b2e486f5d6bb3c0a9a6b50ac85b
Gregory Marshall <gmarshall@dawan.fr>
2023-06-19 15:10:19 +0200

    moving circle

--- core/src/com/gdx/tutorials/breakout/Breakout.java
+++ core/src/com/gdx/tutorials/breakout/Breakout.java
@@ -1,10 +1,14 @@
 package com.gdx.tutorials.breakout;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class Breakout extends ApplicationAdapter {
     private ShapeRenderer shapeRenderer;
+
+    private int x = 50, y = 50;
 
     @Override
     public void create() {
@@ -13,8 +17,12 @@ public class Breakout extends ApplicationAdapter {
 
     @Override
     public void render() {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        x += 5;
+
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.circle(50, 50, 50);
+        shapeRenderer.circle(x, y, 50);
         shapeRenderer.end();
     }
 }
