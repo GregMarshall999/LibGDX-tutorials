f9554b8ea7f4a3449ba8bcdc0330a1b14920ce61
Gregory Marshall <gmarshall@dawan.fr>
2023-06-19 15:45:48 +0200

    back to normal ball

--- core/src/com/gdx/tutorials/breakout/Breakout.java
+++ core/src/com/gdx/tutorials/breakout/Breakout.java
@@ -9,26 +9,22 @@ import com.gdx.tutorials.breakout.components.GameComponent;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class Breakout extends ApplicationAdapter {
     private ShapeRenderer shapeRenderer;
 
     private List<GameComponent> gameComponents = new ArrayList<>();
-    private Random random = new Random();
 
     @Override
     public void create() {
         shapeRenderer = new ShapeRenderer();
 
-        for (int i = 0; i < 10; i++) {
-            gameComponents.add(new Ball(
-                    random.nextInt(Gdx.graphics.getWidth()),
-                    random.nextInt(Gdx.graphics.getHeight()),
-                    random.nextInt(100),
-                    random.nextInt(15),
-                    random.nextInt(15)));
-        }
+        gameComponents.add(new Ball(
+                Gdx.graphics.getWidth() / 2,
+                Gdx.graphics.getHeight() / 2,
+                10,
+                5,
+                5));
     }
 
     @Override
