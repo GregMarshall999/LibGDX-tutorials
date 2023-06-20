05f18869d46691ec3af1adb7e112dd052b6ad162
Gregory Marshall <gmarshall@dawan.fr>
2023-06-20 15:37:52 +0200

    uturn

--- desktop/src/com/gdx/tutorials/DesktopLauncher.java
+++ desktop/src/com/gdx/tutorials/DesktopLauncher.java
@@ -2,7 +2,7 @@ package com.gdx.tutorials;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.gdx.tutorials.fallingfruits.FallingFruits;
+import com.gdx.tutorials.breakout.Breakout;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
@@ -10,6 +10,6 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("My LibGDX Tutorials");
-		new Lwjgl3Application(new FallingFruits(), config);
+		new Lwjgl3Application(new Breakout(), config);
 	}
 }
