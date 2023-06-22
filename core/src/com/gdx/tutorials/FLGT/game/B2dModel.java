7e46cee02dc787344aa211b4f15d9adc0bfaae4d
Gregory Marshall <gmarshall@dawan.fr>
2023-06-22 16:36:53 +0200

    bodies

--- core/src/com/gdx/tutorials/FLGT/game/B2dModel.java
+++ core/src/com/gdx/tutorials/FLGT/game/B2dModel.java
@@ -2,12 +2,22 @@ package com.gdx.tutorials.FLGT.game;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
+import com.gdx.tutorials.FLGT.game.body.Dynamic;
+import com.gdx.tutorials.FLGT.game.body.Kinematic;
+import com.gdx.tutorials.FLGT.game.body.Static;
 
 public class B2dModel {
     private World world;
 
+    private Dynamic bodyD;
+    private Static bodyS;
+    private Kinematic bodyK;
+
     public B2dModel() {
         world = new World(new Vector2(0, -10f), true);
+        bodyD = new Dynamic(world);
+        bodyS = new Static(world);
+        bodyK = new Kinematic(world);
     }
 
     public World getWorld() {
