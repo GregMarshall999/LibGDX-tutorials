324dd0cc629a997d6407ef51640205898425a7ff
Gregory Marshall <gmarshall@dawan.fr>
2023-06-22 11:38:21 +0200

    main menu screen

--- core/src/com/gdx/tutorials/FLGT/display/screen/MenuScreen.java
+++ core/src/com/gdx/tutorials/FLGT/display/screen/MenuScreen.java
@@ -2,14 +2,18 @@ package com.gdx.tutorials.FLGT.display.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.gdx.tutorials.FLGT.FLGT;
 import com.gdx.tutorials.FLGT.display.AbstractFLGTScreen;
 import com.gdx.tutorials.FLGT.display.FLGTButton;
+import com.gdx.tutorials.FLGT.display.FLGTScreen;
+import com.gdx.tutorials.FLGT.display.ScreenUtil;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,6 +31,7 @@ public class MenuScreen extends AbstractFLGTScreen {
         super(context);
 
         stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
         table = new Table();
         skin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
 
@@ -35,16 +40,35 @@ public class MenuScreen extends AbstractFLGTScreen {
         buttons.put(PREFERENCES, new TextButton(PREFERENCES.getName(), skin));
         buttons.put(EXIT, new TextButton(EXIT.getName(), skin));
 
-        //stage init
-        Gdx.input.setInputProcessor(stage);
-        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1/30f));
-        stage.draw();
+        //button events init
+        buttons.get(NEW_GAME).addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                ScreenUtil.changeScreen(context, FLGTScreen.APPLICATION);
+            }
+        });
+        buttons.get(PREFERENCES).addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                ScreenUtil.changeScreen(context, FLGTScreen.PREFERENCE);
+            }
+        });
+        buttons.get(EXIT).addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.app.exit();
+            }
+        });
+    }
 
+    @Override
+    public void show() {
         //UI table init
         table.setFillParent(true);
         table.setDebug(true);
         stage.addActor(table);
 
+        //adding buttons to table
         table.add(buttons.get(NEW_GAME)).fillX().uniformX();
         table.row().pad(10, 0, 10, 0);
         table.add(buttons.get(PREFERENCES)).fillX().uniformX();
@@ -53,14 +77,12 @@ public class MenuScreen extends AbstractFLGTScreen {
     }
 
     @Override
-    public void show() {
-
-    }
-
-    @Override
     public void render(float delta) {
+        //clear screen
         Gdx.gl.glClearColor(0f, 0f, 0f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        //stage redraw
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1/30f));
         stage.draw();
     }
@@ -87,6 +109,6 @@ public class MenuScreen extends AbstractFLGTScreen {
 
     @Override
     public void dispose() {
-
+        stage.dispose();
     }
 }
